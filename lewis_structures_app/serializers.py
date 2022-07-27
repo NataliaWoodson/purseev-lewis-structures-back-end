0340d7991696cbde32c97462d4a49d2249c69386
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-07-27 17:24:27 -0400

    started trying to make routes

---
+++ lewis_structures_app/serializers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import Electron, Atom, Molecule
+from rest_framework import serializers
+
+class ElectronSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Electron
+        fields = ['electron_id', 'is_paired', 'paired_with', 'atom']
+
+class AtomSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Atom
+        fields = ['atom_id', 'molecule', 'electrons', 'name']
+
+class MoleculeSerializaer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Molecule
+        fields = ['molecule_id', 'molecular_formula']
--- lewis_structures_app/views.py
+++ lewis_structures_app/views.py
@@ -1,18 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
-from .models.molecule import Molecule
-from .models.atom import Atom
-from .models.electron import Electron
+from django.contrib.auth.models import Electron, Atom, Molecule
+from rest_framework import viewsets, permissions
+from lewis_structures_app.serializers import ElectronSerializer, AtomSerializer, MoleculeSerializer
+# from django.shortcuts import render
+# from django.http import HttpResponse, HttpRequest
+# from .models.molecule import Molecule
+# from .models.atom import Atom
+# from .models.electron import Electron
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
 
-def bond(request):
-    '''
-    Request body:
-    {
-    "electrons": [<electron1>,<electron2>]
-    }       
-    '''
-    print(HttpRequest.POST)
+# def bond(request):
+#     '''
+#     Request body:
+#     {
+#     "electrons": [<electron1>,<electron2>]
+#     }       
+#     '''
+#     print(HttpRequest.POST)
+
+class ElectronViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows electrons to be viewed or edited
+    """
+    queryset = Electron.objects.all().order_by('electron_id')
+    serializer_class = ElectronSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class AtomViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows electrons to be viewed or edited
+    """
+    queryset = Atom.objects.all().order_by('atom_id')
+    serializer_class = AtomSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class MoleculeViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows electrons to be viewed or edited
+    """
+    queryset = Molecule.objects.all().order_by('molecule_id')
+    serializer_class = MoleculeSerializer
+    permission_classes = [permissions.IsAuthenticated]
