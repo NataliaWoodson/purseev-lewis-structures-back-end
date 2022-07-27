ddf56a7d6864016f6609b27a64edebf2d202cf24
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-07-27 14:35:59 -0400

    deleting extraneous files

--- lewis_structures_app/models/molecule.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Molecule(models.Model):
-    molecule_id = models.BigAutoField(primary_key=True)
-    molecular_formula = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.molecular_formula
