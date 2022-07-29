9cef4865f89d3bf9fd4c069ccbecbb6f0df57f03
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-07-29 16:21:38 -0400

    deploying to AWS eb for the first time

--- lewis_structures_backend/settings.py
+++ lewis_structures_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-pbeg5gw3.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
