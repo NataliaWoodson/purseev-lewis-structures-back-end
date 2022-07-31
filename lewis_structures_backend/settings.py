be4ac8d3f1823c689714dc6db4b55a43c751b102
Jenny C <jennyhc888@gmail.com>
2022-07-31 15:38:54 -0700

    change database settings

--- lewis_structures_backend/settings.py
+++ lewis_structures_backend/settings.py
@@ -83,10 +83,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "purseev_lewis_structures",
-        # "USER": "postgres",
-        # "PASSWORD": "postgres",
-        # "HOST": "",
-        # "PORT": "",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "",
+        "PORT": "",
     }
 }
 import dj_database_url
